1affd12904b659917ba8cc4226b8d28a88a547d1
ArthurStam <bumbledos@gmail.com>
2016-06-06 02:46:42 +0300

    new cities...

--- dev/views/how.js
+++ dev/views/how.js
@@ -19,18 +19,21 @@ let cities = [{
 	coords: [68.969563, 33.07454],
 	name: 'Мурманск',
 	points: [{
+		coords: [68.95147, 33.103143],
 		name: 'Мурманская Областная Станция Переливания Крови',
 		address: 'ул. Павлова, 6',
 		time: 'предварительная запись с 15 до 18.00',
 		phone: '8 (8152) 25-02-61 (62 доктора)',
 		info: 'Принимают только доноров, на типирование можно сдать со второго раза, процедуру объясняют при записи'
 	}, {
+		coords: [68.925654, 33.107868],
 		name: 'CMD – Центр молекулярной диагностики',
 		address: 'Пр. Кольский, д. 61',
 		time: 'до сентября 2016 прием приостановлен, только группы от 20 человек',
 		phone: '8 (8152) 20-77-68',
 		info: 'до сентября 2016 прием приостановлен, только группы от 20 человек'
 	}, {
+		coords: [69.015151, 33.103313],
 		name: 'CMD – Центр молекулярной диагностики',
 		address: 'Ул. Лобова, д. 4',
 		time: 'до сентября 2016 прием приостановлен, только группы от 20 человек',
@@ -42,28 +45,78 @@ let cities = [{
 	coords: [59.939095, 30.315868],
 	name: 'Санкт-Петербург',
 	points: [{
+		coords: [59.96566, 30.324968],
 		name: 'Институт детской гематологии и трансплантологии им. Р.М. Горбачевой',
 		address: 'Ст.м. Петроградская, ул. Рентгена, 12, 10 эт., каб. 1007',
 		time: 'вторник 16:00 - 19:00, четверг 10:00 - 12:00',
 		info: 'Записываться заранее не нужно, просто приходите' 
 	}, {
+		coords: [59.964273, 30.321348],
 		name: 'ПСПбГМУ им. Павлова',
 		address: 'Ст.м. Петроградская, ул. Льва Толстого д. 19, корп. 53',
 		time: 'понедельник - четверг, с 8.30 до 11.30',
 		phone: '+7 (812) 429-24-13',
 		info: 'При себе иметь паспорт с пропиской в любом городе РФ' 
 	}, {
+		coords: [59.83839, 30.418231],
 		name: 'Санкт-Петербургская детская инфекционная больница № 5 им. Н.Ф.Филатова',
 		address: 'Ст. м. Купчино, ул. Бухарестская, д. 134',
 		time: 'понедельник - пятница, с 9.00 до 12.00',
 		phone: '+7 (812) 366-71-66',
 		info: 'При себе иметь паспорт с пропиской в любом городе РФ' 
 	}, {
+		coords: [59.972298, 30.279091],
 		name: 'Городская клиническая больница № 31',
 		address: 'Ст.м. Крестовский остров, пр. Динамо, д. 3',
 		time: 'понедельник, вторник и четверг с 8.30 до 12.00, вторник и четверг с 15.00 до 18.00. В среду ОПК принимают организованные группы доноров по предварительной записи',
 		phone: '+7 (812) 235-73-81',
 		info: 'При себе иметь паспорт с пропиской в любом городе РФ' 
+	}]
+}, {
+	value: 'msk',
+	coords: [55.75396, 37.620393],
+	name: 'Москва',
+	points: [{
+		coords: [55.543766, 37.539922],
+		name: 'Медицинская клиника LeVita',
+		address: 'Ул. Южнобутовская, д. 10 (ст. метро «Бульвар Адмирала Ушакова»)',
+		time: 'пн-чт 12:00 - 16:00, Пт 14:00 - 17:00',
+		phone: '+7 (495) 505-5078, +7 (499) 793-2381, +7 (499) 793-2336, +7 (499) 793-2427',
+		info: 'Записаться можно на сайте клиники www.levita-med.ru, www.levita-kids.ru'
+	}, {
+		coords: [55.801334, 37.552058],
+		name: 'Гематологический научный центр Минздрава России',
+		address: 'Новый Зыковский проезд, д.4А (ст. метро «Динамо»)',
+		time: 'пн-пт 8:00 - 14:00',
+		phone: '+7 (905) 568-5760, +7 (903) 128-84-18'
+	}]
+}, {
+	value: 'kzn',
+	coords: [55.798551, 49.106324],
+	name: 'Казань',
+	points: [{
+		coords: [55.783251, 49.126734],
+		name: 'Казанский филиал Кировского регистра',
+		address: 'ул. Островского, 69/3',
+		time: 'будние дни с 8 до 12',
+		phone: '+7 (843) 292-12-02'
+	}, {
+		coords: [55.72857, 49.1788],
+		name: 'Детская республиканская клиническая больница',
+		address: 'Оренбургский тракт, 140, 1 корпус, Отделение переливания крови, 3-й этаж поликлиники',
+		time: 'будние дни 8:30 – 12:00',
+		phone: '+7 (843) 267-89-20'
+	}, {
+		coords: [55.73002, 49.188655],
+		name: 'РКБ',
+		address: 'Оренбургский тракт, 138',
+		phone: '+7 (843) 237-35-36',
+	}, {
+		coords: [55.753169, 49.171075],
+		name: 'Межрегиональный клинико-диагностический центр (МКДЦ)',
+		address: 'Ул. Карбышева, 12 А, Отделение переливания крови, Блок Б, 1 этаж',
+		time: '8:00 – 12:00',
+		phone: '+7 (843) 291-10-97, +7 (843) 291-10-75'
 	}]
 }];
 
@@ -101,11 +154,12 @@ export default class extends PageView {
 					this.render({ city: city });
 
 					this.$el.find('[data-action="city"]').val(cityValue);
-					this.myMap && this.myMap.destroy();
-					this.myMap = new ymaps.Map('map', {
+					this._map && this._map.destroy();
+					this._map = new ymaps.Map('map', {
 						center: [this.city.coords[0], this.city.coords[1]],
-						zoom: 10
+						zoom: 4
 					});
+					this._addPlacemarks(city.points);
 				} else {
 					this.render({ error: 1 })
 				}
@@ -143,6 +197,25 @@ export default class extends PageView {
 		super._appended();
 	}
 
+	_addPlacemarks(points) {
+		let coords = _.pluck(points, 'coords');
+
+		let collection = new ymaps.GeoObjectCollection({}, {
+			preset: 'islands#redIcon'
+		});
+
+		for (let i = 0; i < coords.length; i++) {
+			let placemark = new ymaps.Placemark(coords[i]) 
+			placemark.properties.set('balloonContent', 
+				`${points[i].address}<br>${points[i].name}`
+			);
+			collection.add(placemark);
+		}
+
+		this._map.geoObjects.add(collection);
+		this._map.setBounds(this._map.geoObjects.getBounds());
+	}
+
 	_prepareData(data = {}) {
 		_.extend(data, {
 			page: page,
@@ -157,9 +230,10 @@ export default class extends PageView {
 		if (data.error) {
 			return data
 		} else if (data.city) {
-			data.firstPoint = data.city.points.shift();
-			data.oddPoints = _.filter(data.city.points, (point, index) => index % 2);
-			data.evenPoints = _.filter(data.city.points, (point, index) => ! (index % 2) );
+			let points = _.clone(data.city.points)
+			data.firstPoint = points.shift();
+			data.oddPoints = _.filter(points, (point, index) => index % 2);
+			data.evenPoints = _.filter(points, (point, index) => ! (index % 2) );
 		}
 
 		return data;
--- static/app.js
+++ static/app.js
@@ -13836,18 +13836,21 @@
 		coords: [68.969563, 33.07454],
 		name: 'Мурманск',
 		points: [{
+			coords: [68.95147, 33.103143],
 			name: 'Мурманская Областная Станция Переливания Крови',
 			address: 'ул. Павлова, 6',
 			time: 'предварительная запись с 15 до 18.00',
 			phone: '8 (8152) 25-02-61 (62 доктора)',
 			info: 'Принимают только доноров, на типирование можно сдать со второго раза, процедуру объясняют при записи'
 		}, {
+			coords: [68.925654, 33.107868],
 			name: 'CMD – Центр молекулярной диагностики',
 			address: 'Пр. Кольский, д. 61',
 			time: 'до сентября 2016 прием приостановлен, только группы от 20 человек',
 			phone: '8 (8152) 20-77-68',
 			info: 'до сентября 2016 прием приостановлен, только группы от 20 человек'
 		}, {
+			coords: [69.015151, 33.103313],
 			name: 'CMD – Центр молекулярной диагностики',
 			address: 'Ул. Лобова, д. 4',
 			time: 'до сентября 2016 прием приостановлен, только группы от 20 человек',
@@ -13859,28 +13862,78 @@
 		coords: [59.939095, 30.315868],
 		name: 'Санкт-Петербург',
 		points: [{
+			coords: [59.96566, 30.324968],
 			name: 'Институт детской гематологии и трансплантологии им. Р.М. Горбачевой',
 			address: 'Ст.м. Петроградская, ул. Рентгена, 12, 10 эт., каб. 1007',
 			time: 'вторник 16:00 - 19:00, четверг 10:00 - 12:00',
 			info: 'Записываться заранее не нужно, просто приходите'
 		}, {
+			coords: [59.964273, 30.321348],
 			name: 'ПСПбГМУ им. Павлова',
 			address: 'Ст.м. Петроградская, ул. Льва Толстого д. 19, корп. 53',
 			time: 'понедельник - четверг, с 8.30 до 11.30',
 			phone: '+7 (812) 429-24-13',
 			info: 'При себе иметь паспорт с пропиской в любом городе РФ'
 		}, {
+			coords: [59.83839, 30.418231],
 			name: 'Санкт-Петербургская детская инфекционная больница № 5 им. Н.Ф.Филатова',
 			address: 'Ст. м. Купчино, ул. Бухарестская, д. 134',
 			time: 'понедельник - пятница, с 9.00 до 12.00',
 			phone: '+7 (812) 366-71-66',
 			info: 'При себе иметь паспорт с пропиской в любом городе РФ'
 		}, {
+			coords: [59.972298, 30.279091],
 			name: 'Городская клиническая больница № 31',
 			address: 'Ст.м. Крестовский остров, пр. Динамо, д. 3',
 			time: 'понедельник, вторник и четверг с 8.30 до 12.00, вторник и четверг с 15.00 до 18.00. В среду ОПК принимают организованные группы доноров по предварительной записи',
 			phone: '+7 (812) 235-73-81',
 			info: 'При себе иметь паспорт с пропиской в любом городе РФ'
+		}]
+	}, {
+		value: 'msk',
+		coords: [55.75396, 37.620393],
+		name: 'Москва',
+		points: [{
+			coords: [55.543766, 37.539922],
+			name: 'Медицинская клиника LeVita',
+			address: 'Ул. Южнобутовская, д. 10 (ст. метро «Бульвар Адмирала Ушакова»)',
+			time: 'пн-чт 12:00 - 16:00, Пт 14:00 - 17:00',
+			phone: '+7 (495) 505-5078, +7 (499) 793-2381, +7 (499) 793-2336, +7 (499) 793-2427',
+			info: 'Записаться можно на сайте клиники www.levita-med.ru, www.levita-kids.ru'
+		}, {
+			coords: [55.801334, 37.552058],
+			name: 'Гематологический научный центр Минздрава России',
+			address: 'Новый Зыковский проезд, д.4А (ст. метро «Динамо»)',
+			time: 'пн-пт 8:00 - 14:00',
+			phone: '+7 (905) 568-5760, +7 (903) 128-84-18'
+		}]
+	}, {
+		value: 'kzn',
+		coords: [55.798551, 49.106324],
+		name: 'Казань',
+		points: [{
+			coords: [55.783251, 49.126734],
+			name: 'Казанский филиал Кировского регистра',
+			address: 'ул. Островского, 69/3',
+			time: 'будние дни с 8 до 12',
+			phone: '+7 (843) 292-12-02'
+		}, {
+			coords: [55.72857, 49.1788],
+			name: 'Детская республиканская клиническая больница',
+			address: 'Оренбургский тракт, 140, 1 корпус, Отделение переливания крови, 3-й этаж поликлиники',
+			time: 'будние дни 8:30 – 12:00',
+			phone: '+7 (843) 267-89-20'
+		}, {
+			coords: [55.73002, 49.188655],
+			name: 'РКБ',
+			address: 'Оренбургский тракт, 138',
+			phone: '+7 (843) 237-35-36'
+		}, {
+			coords: [55.753169, 49.171075],
+			name: 'Межрегиональный клинико-диагностический центр (МКДЦ)',
+			address: 'Ул. Карбышева, 12 А, Отделение переливания крови, Блок Б, 1 этаж',
+			time: '8:00 – 12:00',
+			phone: '+7 (843) 291-10-97, +7 (843) 291-10-75'
 		}]
 	}];
 	
@@ -13963,6 +14016,24 @@
 				_get(Object.getPrototypeOf(_class.prototype), '_appended', this).call(this);
 			}
 		}, {
+			key: '_addPlacemarks',
+			value: function _addPlacemarks(points) {
+				var coords = _underscore2.default.pluck(points, 'coords');
+	
+				var collection = new ymaps.GeoObjectCollection({}, {
+					preset: 'islands#redIcon'
+				});
+	
+				for (var i = 0; i < coords.length; i++) {
+					var placemark = new ymaps.Placemark(coords[i]);
+					placemark.properties.set('balloonContent', points[i].address + '<br>' + points[i].name);
+					collection.add(placemark);
+				}
+	
+				this._map.geoObjects.add(collection);
+				this._map.setBounds(this._map.geoObjects.getBounds());
+			}
+		}, {
 			key: '_prepareData',
 			value: function _prepareData() {
 				var data = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
@@ -13980,11 +14051,12 @@
 				if (data.error) {
 					return data;
 				} else if (data.city) {
-					data.firstPoint = data.city.points.shift();
-					data.oddPoints = _underscore2.default.filter(data.city.points, function (point, index) {
+					var points = _underscore2.default.clone(data.city.points);
+					data.firstPoint = points.shift();
+					data.oddPoints = _underscore2.default.filter(points, function (point, index) {
 						return index % 2;
 					});
-					data.evenPoints = _underscore2.default.filter(data.city.points, function (point, index) {
+					data.evenPoints = _underscore2.default.filter(points, function (point, index) {
 						return !(index % 2);
 					});
 				}
@@ -14012,11 +14084,12 @@
 							_this5.render({ city: city });
 	
 							_this5.$el.find('[data-action="city"]').val(cityValue);
-							_this5.myMap && _this5.myMap.destroy();
-							_this5.myMap = new ymaps.Map('map', {
+							_this5._map && _this5._map.destroy();
+							_this5._map = new ymaps.Map('map', {
 								center: [_this5.city.coords[0], _this5.city.coords[1]],
-								zoom: 10
+								zoom: 4
 							});
+							_this5._addPlacemarks(city.points);
 						} else {
 							_this5.render({ error: 1 });
 						}
